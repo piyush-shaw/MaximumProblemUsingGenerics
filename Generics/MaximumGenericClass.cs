5e9f7cea3d1c7e19e7e3e020175f31052d7b44f2
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-17 23:17:54 +0530

    [Piyush] added to refactor for creating generic class to take in 3variables of generic type

---
+++ Generics/MaximumGenericClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Generics
+{
+    //Refactor to create generic class to take in 3 variables of generic type
+
+    public class MaximumGenericClass<T> where T : IComparable
+	{
+        //these are generic variables
+		public T firstValue, secondValue, thirdValue;
+
+		public MaximumGenericClass(T firstValue,T secondValue,T thirdValue)
+		{
+			this.firstValue = firstValue;
+			this.secondValue = secondValue;
+			this.thirdValue = thirdValue;
+		}
+        //Refactor all 3 method in to one generic method and finding maximum value
+        public static T MaxValue(T firstValue, T secondValue, T thirdValue) 
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+               firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+               firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 ||
+                thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 ||
+                thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) >= 0)
+            {
+                return thirdValue;
+            }
+            return default;
+        }
+    }
+}
+
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -5,7 +5,8 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Maximum Problem Using Generics ");
-        Console.WriteLine("\n1.Maximum Integer \n2.Maximum Floating \n3.Maximum String \n4.Maximum Using Generic Method ");
+        Console.WriteLine("\n1.Maximum Integer \n2.Maximum Floating \n3.Maximum String \n4.Maximum Using Generic Method "
+            + "\n5.Maximum Using Generic Class");
         int option = Convert.ToInt32(Console.ReadLine());
         switch (option)
         {
@@ -26,6 +27,12 @@ internal class Program
                 Console.WriteLine("Maximum For Integer "+MaximumGenericMethod.MaxValue(10,15,11));
                 Console.WriteLine("Maximum For Float "+ MaximumGenericMethod.MaxValue(9.1f,7.9f,8.7f));
                 Console.WriteLine("Maximum For String "+ MaximumGenericMethod.MaxValue("Bengaluru","Delhi","Kolkata"));
+                break;
+            case 5:
+                Console.WriteLine("Maximum using Generic Class");
+                Console.WriteLine("Maximum For Integer " + MaximumGenericClass<int>.MaxValue(100, 115, 211));
+                Console.WriteLine("Maximum For Float " + MaximumGenericClass<float>.MaxValue(19.1f, 27.9f, 18.7f));
+                Console.WriteLine("Maximum For String " + MaximumGenericClass<string>.MaxValue("Apple", "Peach", "Banana"));
                 break;
             default:
                 Console.WriteLine("Invalid option ");
