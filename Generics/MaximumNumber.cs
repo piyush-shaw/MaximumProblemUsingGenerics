8bd9beaff0972a61d45be41916f8a1059dee34a7
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-17 17:19:19 +0530

    [Piyush] added to find maximum floating number among three float number

--- Generics/MaximumNumber.cs
+++ Generics/MaximumNumber.cs
@@ -3,6 +3,7 @@ namespace Generics
 {
 	class MaximumNumber
 	{
+        //UC 1 - for checking maximum integer number
 		public static int MaximumIntegerNumber(int firstValue,int secondValue,int thirdValue)
 		{
 			if(firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
@@ -25,6 +26,29 @@ namespace Generics
 			}
 			return firstValue;
         }
-	}
+        //UC 2 - for checking maximum floating number
+        public static double MaximumFloatNumber(double firstValue, double secondValue, double thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 ||
+                thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 ||
+                thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) >= 0)
+            {
+                return thirdValue;
+            }
+            return firstValue;
+        }
+    }
 }
 
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -5,13 +5,17 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Maximum Problem Using Generics ");
-        Console.WriteLine("\n1.Maximum Integer");
+        Console.WriteLine("\n1.Maximum Integer \n2.Maximum Floating");
         int option = Convert.ToInt32(Console.ReadLine());
         switch (option)
         {
             case 1:
-                Console.WriteLine();
+                Console.WriteLine("Maximum Integer Number");
                 Console.WriteLine(MaximumNumber.MaximumIntegerNumber(25,48,41));
+                break;
+            case 2:
+                Console.WriteLine("Maximum Floating Number");
+                Console.WriteLine(MaximumNumber.MaximumFloatNumber(8.6,5.9,9.8));
                 break;
             default:
                 Console.WriteLine("Invalid option ");
