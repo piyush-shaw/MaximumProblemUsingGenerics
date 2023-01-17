2c338f0869acc4691e6ee08910eab8080fee0145
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-17 17:00:56 +0530

    [Piyush] added three integer number find maximum among them

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,7 +1,21 @@
-﻿internal class Program
+﻿using Generics;
+
+internal class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Maximum Problem Using Generics ");
+        Console.WriteLine("\n1.Maximum Integer");
+        int option = Convert.ToInt32(Console.ReadLine());
+        switch (option)
+        {
+            case 1:
+                Console.WriteLine();
+                Console.WriteLine(MaximumNumber.MaximumIntegerNumber(25,48,41));
+                break;
+            default:
+                Console.WriteLine("Invalid option ");
+                break;
+        }
     }
 }
